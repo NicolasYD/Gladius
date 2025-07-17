72c147ee1e96e5c965ab03aad3072a087c9e4b8b
NicolasYD <147659516+NicolasYD@users.noreply.github.com>
2025-07-17 15:32:36 +0200

    Added headers.

--- Modules/Tags.lua
+++ Modules/Tags.lua
@@ -1,3 +1,9 @@
+-- @@@@@@@@@@@@@@@@@@@@@@@@@@@@ Tags Module @@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+-- Originally written by: Resike and Firebunny. Original author: Proditor
+-- Modified by: Pharmac1st
+-- Game Version: 11.1.7
+-- @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
 local Gladius = _G.Gladius
 if not Gladius then
 	DEFAULT_CHAT_FRAME:AddMessage(format("Module %s requires Gladius", "Tags"))
